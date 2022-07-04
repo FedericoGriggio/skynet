18f1533968c214d4b8d63d68d8abc321e933ecc7
Federico Griggio <griggio.federico@gmail.com>
2022-07-04 14:13:50 +0200

    Initial Push

---
+++ skynet/lib.py
@@ -0,0 +1,4 @@
+# skynet/lib.py
+
+def try_me():
+    return 'Florence Langer, a Resistance soldier, travel to the present day from the Future War to murder Alex Murphy as she had been told by Connor that it was a human mind that turned Skynet from a tool to an "army of murders". During a brief confrontation, the group was attacked by a group of Series 800 terminators. Murphy soon learned that he was a key component of the Skynet program and that his neural network gave Skynet the ability to develop artificial intelligence. Rather than be an agent of destruction, Murphy opted instead to terminate himself. While visiting his grave a final time he is attacked by a modified Infiltrator resembling a child and another that looks like a dog. Murphy is disabled and his program scanned into Skynet.'
